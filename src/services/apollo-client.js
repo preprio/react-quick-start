a56cbb8cf304fdff2a10494f451b04171fd720d3
Kevintjuhz <k.quaedvlieg@outlook.com>
2023-06-13 10:11:03 +0200

    React added access token to apollo client

--- src/services/apollo-client.js
+++ src/services/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: `https://graphql.prepr.io/${process.env.REACT_APP_PREPR_ACCESS_TOKEN}`,
+  uri: `https://graphql.prepr.io/${process.env.REACT_APP_PREPR_ACCESS_TOKEN || '7f05e9e2f17f1b3d5e08dab3a565acbea8b87745473917e159f70ae1cf0334b9'}`,
   cache: new InMemoryCache(),
 });
 
